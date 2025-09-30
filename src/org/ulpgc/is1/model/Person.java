8377803267e49d05c2199a5e4c8475b7d022e982
Kolocorleone <marcosgrancanaria200@gmail.com>
2025-09-30 15:45:24 +0100

    prueba

--- src/org/ulpgc/is1/model/Person.java
+++ src/org/ulpgc/is1/model/Person.java
@@ -1,5 +1,17 @@
 package org.ulpgc.is1.model;
 
 public class Person extends Contact {
+    private String firstName;
+    private String lastName;
+
+    public Person(String telephone, String email, String firstName, String lastName) {
+        super(telephone, email);
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
 
 }
